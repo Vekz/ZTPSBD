71d3c7a982c1d4d560163c4f66a9d4408dcaaeb7
WolfgangPolska <wolfgangpolska@gmail.com>
2020-11-28 16:58:12 +0100

    Added logout button got cancer

---
+++ Pages/Login/UserLogout.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZTPSBD.Pages.Login
+{
+    public class UserLogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
